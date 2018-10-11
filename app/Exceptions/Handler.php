14c34f5609bafe38d158f1eb743fe0e0bc48297f
JuanDMeGon <juandmegon@gmail.com>
2018-10-11 16:06:54 -0500

    Protegiendo las rutas del Gateway con Lumen Passport

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use Exception;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Response;
 use GuzzleHttp\Exception\ClientException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
