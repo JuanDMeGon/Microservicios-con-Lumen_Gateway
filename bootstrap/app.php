6b8ea641e192cbd6354e094e5867a28b2fdbc113
JuanDMeGon <juandmegon@gmail.com>
2018-10-11 15:08:55 -0500

    Instalando y habilitando los componentes de Lumen passport

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -83,9 +83,11 @@ $app->singleton(
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
