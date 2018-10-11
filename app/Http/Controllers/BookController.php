b3d90f19b23ccd3fa5241aeb36591cfc3ca0ba2b
JuanDMeGon <juandmegon@gmail.com>
2018-10-10 21:27:13 -0500

    Implementando las operaciones para libros basados en las de autor

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use App\Services\BookService;
+use Illuminate\Http\Response;
 
 class BookController extends Controller
 {
@@ -27,43 +28,48 @@ class BookController extends Controller
     }
 
     /**
-     * Retrieve and show all the books
+     * Retrieve and show all the Books
      * @return Illuminate\Http\Response
      */
     public function index()
     {
+        return $this->successResponse($this->bookService->obtainBooks());
     }
 
     /**
-     * Creates an instance of book
+     * Creates an instance of Book
      * @return Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        return $this->successResponse($this->bookService->createBook($request->all()), Response::HTTP_CREATED);
     }
 
     /**
-     * Obtain and show an instance of book
+     * Obtain and show an instance of Book
      * @return Illuminate\Http\Response
      */
     public function show($book)
     {
+        return $this->successResponse($this->bookService->obtainBook($book));
     }
 
     /**
-     * Updated an instance of book
+     * Updated an instance of Book
      * @return Illuminate\Http\Response
      */
     public function update(Request $request, $book)
     {
+        return $this->successResponse($this->bookService->editBook($request->all(), $book));
     }
 
     /**
-     * Removes an instance of book
+     * Removes an instance of Book
      * @return Illuminate\Http\Response
      */
     public function destroy($book)
     {
+        return $this->successResponse($this->bookService->deleteBook($book));
     }
 
 }
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -9,7 +9,7 @@ class BookService
     use ConsumesExternalService;
 
     /**
-     * The base uri to be used to consume the authors service
+     * The base uri to be used to consume the books service
      * @var string
      */
     public $baseUri;
@@ -17,6 +17,51 @@ class BookService
     public function __construct()
     {
         $this->baseUri = config('services.books.base_uri');
+    }
+
+    /**
+     * Get the full list of books from the books service
+     * @return string
+     */
+    public function obtainBooks()
+    {
+        return $this->performRequest('GET', '/books');
+    }
+
+    /**
+     * Create an instance of book using the books service
+     * @return string
+     */
+    public function createBook($data)
+    {
+        return $this->performRequest('POST', '/books', $data);
+    }
+
+    /**
+     * Get a single book from the books service
+     * @return string
+     */
+    public function obtainBook($book)
+    {
+        return $this->performRequest('GET', "/books/{$book}");
+    }
+
+    /**
+     * Edit a single book from the books service
+     * @return string
+     */
+    public function editBook($data, $book)
+    {
+        return $this->performRequest('PUT', "/books/{$book}", $data);
+    }
+
+    /**
+     * Remove a single book from the books service
+     * @return string
+     */
+    public function deleteBook($book)
+    {
+        return $this->performRequest('DELETE', "/books/{$book}");
     }
 
 }
